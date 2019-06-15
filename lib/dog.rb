9a9df67385990cd705046198a6393d8f0c9320d5
Donna Amos <donnacamos.developer@gmail.com>
2019-06-15 21:50:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,8 @@ class Dog
   attr_accessor :id, :name, :breed,  
   
   def initialize(attributes) 
-    attributes.each { |key, value| self.send (("#{key=}"), value)} 
+    attributes.each { |key, value| self.send (("#{key=}"), value)}
+    
   end 
     
 end 
